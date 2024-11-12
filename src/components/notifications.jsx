7f27efe2f65aca4ba4e6d9ced6e151266d245f86
grantcollins1 <116646023+grantcollins1@users.noreply.github.com>
2024-11-12 10:39:54 -0500

    ok

--- src/components/notifications.jsx
+++ src/components/notifications.jsx
@@ -5,7 +5,18 @@ const Notifications = () => {
     const [notifications, setNotis] = useState([]);
     useEffect( () => {
         const getNotifications = async () =>  {
-            const resp = await fetch('/api/notifications');
+            const resp = [{
+                id: 1,
+                title: 'My first blog post',
+                text: 'Hello!',
+                published_at: new Date('2020-10-23'),
+            },
+            {
+                id: 2,
+                title: 'Updating my blog',
+                text: 'second',
+                published_at: new Date('2020-10-26'),
+            } ]
             const notiResp = await resp.json();
             setNotis(notiResp);
         };
