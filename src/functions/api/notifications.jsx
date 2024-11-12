f5a3f2e8f6346f4caf9fb5f3b2406930500cfce1
grantcollins1 <116646023+grantcollins1@users.noreply.github.com>
2024-11-12 16:13:36 -0500

    binding KV

--- src/functions/api/notifications.jsx
+++ src/functions/api/notifications.jsx
@@ -3,3 +3,14 @@ import notifications from './notification/data.jsx'
 export function onRequestGet() {
     return Response.json(notifications)
 }
+
+async function handleRequest(request) {
+    const defaultData = {
+                "type": "success",
+                "content": {
+                    "text" : "testing"
+                },
+                "read": false
+    }
+    NOTIFICATIONS.put("notifications", JSON.stringify(defaultData))
+}
