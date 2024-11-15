27ee3b4cea5e5d2c71fc27aa39af32409dbab5c8
grantcollins1 <116646023+grantcollins1@users.noreply.github.com>
2024-11-14 22:16:17 -0500

    simplified test for post

--- functions/api/testing.jsx
+++ functions/api/testing.jsx
@@ -48,7 +48,7 @@ export async function onRequestPost(context, request) {
     const data = await context.env.NOTIFICATIONS.get("notifications")
     const parsed_data = JSON.parse(data)
     parsed_data.push(body)
-      await context.env.NOTIFICATIONS.put("notifications", JSON.stringify(parsed_data));
+      await context.env.NOTIFICATIONS.put("notifications", JSON.stringify(body));
       return new Response(data, { status: 200 })
     } catch (err) {
       return new Response(err, { status: 500 })
