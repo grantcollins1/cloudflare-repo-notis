2e6e0cbc19048f08867c99db9deebfb31e8bc3e1
grantcollins1 <116646023+grantcollins1@users.noreply.github.com>
2024-11-14 19:12:44 -0500

    please! again

--- functions/api/testing.jsx
+++ functions/api/testing.jsx
@@ -1,5 +1,48 @@
-import samples from '../../src/notification/data';
+onst samples = [
+    {
+        "type": "success",
+        "content": {
+            "text" : "test1"
+        },
+        "read": false
+  },
+    {
+        "type": "alert",
+        "content": {
+            "text" : "ok den"
+        },
+        "read": false
+    },
+    {
+        "type": "alert",
+        "content": {
+            "text" : "ok den"
+        },
+        "read": false
+    },
+    {
+        "type": "alert",
+        "content": {
+            "text" : "ok den"
+        },
+        "read": false
+    },
+    {
+        "type": "info",
+        "content": {
+            "text" : "ok yeah"
+        },
+        "read": false
+    },
+    {
+        "type": "alert",
+        "content": {
+            "text" : "ok den"
+        },
+        "read": false
+    }
+];
 export async function onRequestPost(context) {
-    await ontext.env.NOTIFICATIONS.put("notifications", samples);
-    return new Response("Hello from workers!")
+    await context.env.NOTIFICATIONS.put("notifications", samples);
+    return new Response("Hello from workers!");
 }
